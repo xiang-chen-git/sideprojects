90b88d10f4f0f068ffaad2e0afa0c3fbbe1dd232
xiang-chen-git <xiangchen2016@gmail.com>
2016-05-23 15:57:56 -0400

    update

--- Text-1.cpp
+++ Text-1.cpp
@@ -9,3 +9,40 @@
 
 
 using namespace std;
+
+
+vector<vector<int>> fourSum(vector<int>&num,int target){
+    vector<vector<int>> result;
+    if(num.size()<4) return result;
+    sort(num.begin(),num.end());
+    
+    auto last = num.end();
+    auto first = num.begin();
+    for(auto i=first;i<prev(last,3);i++){
+        for(auto j=next(i);j<prev(last,2);j++){
+            auto k =next(j);
+            auto h =prev(last);
+            while(k<h){
+                auto sum = *i+*j+*k+*h;
+                if(sum<target){
+                    k++;
+                }else if(sum>target){
+                    h--;
+                }else{
+                    result.push_back({*i,*j,*k,*h});
+                    k++;h--;
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    sort(result.begin(),result.end());
+    result.erase(unique(result.begin(),result.end()),result.end());
+    
+    return result;
+}
